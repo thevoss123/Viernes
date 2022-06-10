fe3371dfc7ba30caf802972ce0279a2bbba9632b
thevoss123 <106287134+thevoss123@users.noreply.github.com>
2022-06-10 17:43:31 -0500

    Add files via upload

---
+++ 4a.js
@@ -0,0 +1,21 @@
+fetch("codigo.json")
+.then(function(response){
+    return response.json();
+})
+.then(function(codigo){
+    let placeholder=document.querySelector("#data-output");
+    let out="";
+    for(let dia of codigo){
+        out+=`
+        <tr>
+            <td>${dia.id}</td>
+            <td>${dia.nombre}</td>
+            <td>${dia.edad}</td>
+            <td>${dia.identidadsecreta}</td>
+            <td>${dia.poderes}</td>
+            <td><img src='${dia.imagen}' width="200px" height="200px"></td>
+        </tr>
+        `
+    }
+    placeholder.innerHTML=out;
+})
